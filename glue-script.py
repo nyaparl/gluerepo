42f3a038c4000230e37e1d259d96130604158fde
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-05-30 22:36:00 +0530

    Create glue-script.py

---
+++ glue-script.py
@@ -0,0 +1,11 @@
+import allImports
+import sessionCreation
+
+spark = createSession()
+
+input_path = "s3://my-bucket-2024-05-30-new/input/emp.txt"
+output_path = "s3://my-bucket-2024-05-30-new/output/emp.json"
+
+df = spark.read.format("csv").load(input_path)
+
+df.coalesce(1).write.mode("overwrite").format("json").save(output_path)
